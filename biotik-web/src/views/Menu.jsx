3a618426903f003c59b11f5eff56286834fd3efd
Leonardo Villagrán <leonardovillagranchicago@gmail.com>
2024-12-09 15:00:34 -0300

    refactor Menu component: update animation and restructure navigation links

--- biotik-web/src/views/Menu.jsx
+++ biotik-web/src/views/Menu.jsx
@@ -1,25 +1,28 @@
 import { motion } from 'framer-motion';
-import { AppBar, Toolbar, Button, Box, Stack } from '@mui/material';
 
 export default function Menu() {
     return (
-        <motion.div
-            initial={{ y: '-100vw', opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 1, type: 'tween' }}
+        <motion.div className="card bg-gradient"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.3, duration: 1, type: 'tween' }}
+
         >
-            <AppBar position="static">
-                <Toolbar>
-                    <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-                        <Stack direction="row" spacing={2}>
-                            <Button color="inherit" href="#advantages">Ventajas</Button>
-                            <Button color="inherit" href="#data-security">Seguridad</Button>
-                            <Button color="inherit" href="#data-processing">Descarga de datos</Button>
-                            <Button color="inherit" href="#why-choose-biotik">¿Por qué BiotiK?</Button>
-                        </Stack>
-                    </Box>
-                </Toolbar>
-            </AppBar>
+            <div className="card-body">
+                <nav className="hero-nav">
+                    <div className="d-flex flex-wrap">
+                        <a className="nav-link col-12 col-sm-4 col-md-4 col-lg-2" href="#about-biotik">¿Qué es BiotiK?</a>
+                        <a className="nav-link col-12 col-sm-4 col-md-4 col-lg-2" href="#how-it-works">¿Cómo funciona?</a>
+                        <a className="nav-link col-12 col-sm-4 col-md-4 col-lg-2" href="#advantages">Ventajas</a>
+                        <a className="nav-link col-12 col-sm-4 col-md-4 col-lg-2" href="#data-security">Seguridad</a>
+                        <a className="nav-link col-12 col-sm-4 col-md-4 col-lg-2" href="#data-processing">Descarga de datos</a>
+                        <a className="nav-link col-12 col-sm-4 col-md-4 col-lg-2" href="#why-choose-biotik">¿Por qué BiotiK?</a>
+                    </div>
+                </nav>
+            </div>
         </motion.div>
-    );
+
+
+
+    )
 }
